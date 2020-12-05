9dbb8d2797977d146248268f8ffde3a9388c76ea
yasindu1997 <yasindusathsaraoth@gmail.com>
2020-12-05 13:23:27 +0530

    Initial commit

---
+++ src/test/java/lk/ijse/instagramclone/InstagramCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.instagramclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
